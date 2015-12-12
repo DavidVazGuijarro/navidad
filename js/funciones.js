015f2acf3abe535297436d9f15866fa0fecf8479
rocimunoz <rocimunoz@gmail.com>
2015-12-12 11:58:57 +0100

    node app

--- js/funciones.js
+++ js/funciones.js
@@ -1,14 +1,30 @@
 
-(function() {
+$( document ).ready(function() {
+	    
+	cargaLuces();
 
 
-setInterval(function(){
-  var el = document.getElementById('blink');
-  if(el.className == 'luz'){
-      el.className = 'luz on';
-  }else{
-      el.className = 'luz';
-  }
-},500);
+	$('#musica').click(function() {
+	  alert( "Handler for .click() called." );
+	});
 
-})();
+
+	function cargaLuces(){
+
+   		setInterval(function(){
+	  		var el = document.getElementById('blink');
+	  		if(el.className == 'luz'){
+	      		el.className = 'luz on';
+	  		}else{
+	      		el.className = 'luz';
+	  		}
+		},500);
+
+	}
+
+	function cargaMusica(){
+
+
+	}
+
+});
